17e1a8edcbf37d0797f927f01f756616d274a7a6
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2022-10-21 17:29:09 +0200

    Remove solution

--- src/getNoiseValue.ts
+++ src/getNoiseValue.ts
@@ -10,5 +10,5 @@ const simplex2 = new tumult.Simplex2(SEED);
   Tar inn 2D-koordinater, og returnerer en verdi mellom -1 og 1.
 */
 export const getNoiseValue = (x: number, y: number): number => {
-  return perlin2.octavate(1, x / MAP_FACTOR, y / MAP_FACTOR);
+  return 0; // TODO Gjør noe mer spennende her!
 };
