95006c16176ed47d0cfdafd50df2757d63961831
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2022-10-21 15:10:05 +0200

    Vite setup

--- scripts/main.mjs
+++
@@ -1,5 +0,0 @@
-import { init as initDots } from "./dots.mjs";
-
-document
-  .getElementById("dots-button")
-  .addEventListener("click", () => initDots());
--- server.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-
-var app = express();
-app.use("/", express.static("."));
-app.listen(8000);
-
-console.log("Listening at http://localhost:8000");
